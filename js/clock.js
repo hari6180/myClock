147e8f3532d31f302708dc0df574a3537f2d956e
hari <ori080525@gmail.com>
2021-07-10 22:46:41 +0900

    add timer function

--- js/clock.js
+++ js/clock.js
@@ -1,4 +1,5 @@
 const clock = document.querySelector("h2#clock");
+const timer = document.querySelector("h2#timer");
 
 function getClock() {
   const date = new Date();
@@ -8,5 +9,24 @@ function getClock() {
   clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
+let min = 0;
+let sec = 0;
+
+function getTimer() {
+  if (sec < 59) {
+    setInterval(sec++, 1000);
+  } else {
+    sec = 0;
+    min++;
+  }
+
+  const minutes = String(min).padStart(2, "0");
+  const seconds = String(sec).padStart(2, "0");
+
+  timer.innerText = `${minutes}:${seconds}`;
+}
+
 getClock();
 setInterval(getClock, 1000);
+
+setInterval(getTimer, 1000);
